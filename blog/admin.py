9c9838d25534aec8effdab873d39f09ead4fffa0
linjunyi <ljyalan@126.com>
2018-03-24 22:46:27 +0800

    修改后台配置

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post,Categoty,Tag
+from .models import Post, Category, Tag
 
 # Register your models here.
 
@@ -11,5 +11,5 @@ password:ljy121800
 
 # 注册models 中的三个数据表
 admin.site.register(Post)
-admin.site.register(Categoty)
+admin.site.register(Category)
 admin.site.register(Tag)
