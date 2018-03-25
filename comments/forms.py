e5231b49a701435b585b1c66768e62fc12831912
linjunyi <ljyalan@126.com>
2018-03-25 22:57:10 +0800

    update .gitignore

--- comments/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Comment
-
-
-# 表单类
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['name', 'email', 'url', 'text']
--- comments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'comments'
-urlpatterns = [
-    path(r'comment/post/<int:post_pk>/', views.post_comment, name='post_comment')
-]
