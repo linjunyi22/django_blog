9173c0ab4799a69b0d72e071a74d4943b336bdeb
linjunyi <ljyalan@126.com>
2018-03-26 22:27:08 +0800

    数据库更新

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # 导入 reverse 函数
 from django.urls import reverse
 from django.utils.html import strip_tags
-
+import markdown
 # Create your models here.
 
 
@@ -76,3 +76,23 @@ class Post(models.Model):
     class Meta:
         ordering = ['-created_time']
 
+    views = models.PositiveIntegerField(default=0)
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            #  实例化一个 markdown 类，用于渲染 body 文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite'
+            ])
+            # 先将 Markdown 文本渲染成 HTML 文本
+            # strip_tags 去掉 HTML 文本的全部 HTML 标签
+            # 从文本摘取前 54 个字符赋给 excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        # 调用父类的 save 方法将数据保存到数据库中
+            super(Post, self).save(*args, **kwargs)
