cdc55f3c5c306eed14a1026628819949497ac35d
linjunyi <ljyalan@126.com>
2018-04-01 18:13:09 +0800

    修改评论应用

--- comments/forms.py
+++ comments/forms.py
@@ -6,4 +6,5 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['name', 'email', 'url', 'text']
+        # fields = ['name', 'email', 'url', 'text']
+        fields = ['name', 'text']
--- comments/models.py
+++ comments/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 class Comment(models.Model):
     '''
-    评论数据表，用于存放评论人名称，email，url，评论内容，评论时间及对应的文章
+    评论数据表，用于存放名称，评论内容，评论时间及对应的文章
     '''
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=255)
-    url = models.URLField(blank=True)
+    # email = models.EmailField(max_length=255)
+    # url = models.URLField(blank=True)
     text = models.TextField()
-    created_time = models.DateField(auto_now_add=True)  # 把 created_time 指定为当前时间
+    created_time = models.DateTimeField(auto_now_add=True)  # 把 created_time 指定为当前时间
 
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE)
 
