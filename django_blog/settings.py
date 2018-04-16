0facc864c98beff96bd405990341466a2b63e57b
linjunyi <ljyalan@126.com>
2018-04-16 21:49:41 +0800

    修改部分配置

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i$!kmbf+=#uc+#0%1#+l-o@fr76k@^#y(t^cu$j&cut2__mq43'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.halolin.fun']
 
